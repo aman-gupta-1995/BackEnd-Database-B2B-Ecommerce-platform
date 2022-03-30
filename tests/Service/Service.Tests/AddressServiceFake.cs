3862ea209b8bb2fa1c76018c119cfb2716935ac9
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-30 12:52:38 +0530

    Update AddressServiceFake.cs

--- tests/Service/Service.Tests/AddressServiceFake.cs
+++ tests/Service/Service.Tests/AddressServiceFake.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using AybCommerce.Core.Interfaces.Services;
 using AybCommerce.Domain.Entities;
-
+ 
 namespace Service.Tests
 {
     public class AddressServiceFake : IAddressService 
