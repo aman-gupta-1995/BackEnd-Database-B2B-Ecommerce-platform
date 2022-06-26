2f8e5739f8b9b8f3c004d53d775a5af7e49b5fbc
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-26 16:59:08 +0530

    Update AuditableEntity.cs

--- src/Domain/AybCommerce.Domain/Base/AuditableEntity.cs
+++ src/Domain/AybCommerce.Domain/Base/AuditableEntity.cs
@@ -2,7 +2,7 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AybCommerce.Domain.Base 
+namespace AybCommerce.Domain.Base  
 {    
     public abstract class AuditableEntity : BaseEntity    
     { 
