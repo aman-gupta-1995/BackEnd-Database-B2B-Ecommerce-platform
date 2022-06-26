c19bd5ecd326e37bcd936a7631e9c1cc41494064
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-26 16:54:28 +0530

    Update DataTableFormRequest.cs

--- src/Common/AybCommerce.Common.Models/DataTableFormRequest.cs
+++ src/Common/AybCommerce.Common.Models/DataTableFormRequest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Http;
  
-namespace AybCommerce.Common.Models
+namespace AybCommerce.Common.Models 
 {   
     public class DataTableFormRequest   
     {   
