acb846f869349f043b5ee7ecd5700d107a7642d8
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-26 16:57:49 +0530

    Update IAddressService.cs

--- src/Core/AybCommerce.Core.Interfaces/Services/IAddressService.cs
+++ src/Core/AybCommerce.Core.Interfaces/Services/IAddressService.cs
@@ -5,7 +5,7 @@ namespace AybCommerce.Core.Interfaces.Services
     public interface IAddressService    
     {    
         // normally they all should return some response model   
-        Address RetrieveAddress(int addressId);  
+        Address RetrieveAddress(int addressId);   
 
         bool UpsertAddress(Address address);
 
