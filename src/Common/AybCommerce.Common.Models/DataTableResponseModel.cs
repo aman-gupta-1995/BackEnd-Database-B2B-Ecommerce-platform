fe30251c500181be6a11b6137483a818013f342c
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-31 10:37:53 +0530

    Update DataTableResponseModel.cs

--- src/Common/AybCommerce.Common.Models/DataTableResponseModel.cs
+++ src/Common/AybCommerce.Common.Models/DataTableResponseModel.cs
@@ -3,7 +3,7 @@
 namespace AybCommerce.Common.Models
 {
     public class DataTableResponseModel<T>
-    { 
+    {  
         public DataTableResponseModel(T data, string draw, int recordsTotal, int recordsFiltered) 
         {  
             Data = data;
