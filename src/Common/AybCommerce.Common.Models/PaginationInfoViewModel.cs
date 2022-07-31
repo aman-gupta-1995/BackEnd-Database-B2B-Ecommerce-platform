07475a241e19906e9796176d910d12d611557275
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-31 10:39:25 +0530

    Update PaginationInfoViewModel.cs

--- src/Common/AybCommerce.Common.Models/PaginationInfoViewModel.cs
+++ src/Common/AybCommerce.Common.Models/PaginationInfoViewModel.cs
@@ -5,7 +5,7 @@
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; } 
         public int ActualPage { get; set; }
-        public int TotalPages { get; set; }  
+        public int TotalPages { get; set; }   
         public string Previous { get; set; }
         public string Next { get; set; }
     }
