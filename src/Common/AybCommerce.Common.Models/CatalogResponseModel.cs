25b6fd48c1bed56792b3a95098869c1a106013a2
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-31 10:36:02 +0530

    Update CatalogResponseModel.cs

--- src/Common/AybCommerce.Common.Models/CatalogResponseModel.cs
+++ src/Common/AybCommerce.Common.Models/CatalogResponseModel.cs
@@ -6,7 +6,7 @@ namespace AybCommerce.Common.Models
     public class CatalogResponseModel  
     {       
         public List<Product> Products { get; set; }       
-  
+   
         public PaginationInfoViewModel PaginationInfo { get; set; }  
 
         public int? CategoryFilterApplied { get; set; }
